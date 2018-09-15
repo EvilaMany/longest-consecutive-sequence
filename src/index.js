62aeb10f9a455e56921669abe57881e9e5e99ce6
EvilaMany <lineman98@yandex.ru>
2018-09-15 20:42:57 +0300

    half of tenth task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+module.exports = 
+function longestConsecutiveLength(array) {
+  if(array.length == 0) return 0;
+  array = array.sort((a,b) => a-b)
+  let last_val = array[0], max_count=1,count = 1;
+
+  for(let num = 1; num < array.length;num++)
+  {
+  	if(array[num]-1 == last_val )//|| array[num] == last_val)
+  	{
+  		last_val = array[num];
+  		count++;
+  	}
+  	else
+  	{
+  		last_val = array[num];
+  		if(count > max_count)
+  			max_count = count;
+  		count = 1;
+  	}
+  }
+  return max_count;
+
 }
